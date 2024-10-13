b7c366c5899aa8e926f0779de69515434ff943c2
umutozgenc34 <umutozgenc34@hotmail.com>
2024-10-13 23:31:13 +0300

    Controller Added

--- HospitalAppointment.WebApi/Controllers/AppointmentsController.cs
+++ HospitalAppointment.WebApi/Controllers/AppointmentsController.cs
@@ -1,11 +1,59 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HospitalAppointment.WebApi.Models;
+using HospitalAppointment.WebApi.Services.Abstracts;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HospitalAppointment.WebApi.Controllers
+namespace HospitalAppointment.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AppointmentsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AppointmentsController : ControllerBase
+    private IAppointmentService _appointmentsService;
+
+    public AppointmentsController(IAppointmentService appointmentService)
     {
+        _appointmentsService = appointmentService;
+    }
+
+    [HttpGet("getall")]
+    public IActionResult GetAll()
+    {
+        var result = _appointmentsService.GetAllAppointments();
+        return Ok(result);
+    }
+
+    [HttpPost("add")]
+    public IActionResult Add(Appointment appointment)
+    {
+        var result = _appointmentsService.AddAppointment(appointment);
+        return Ok(result);
+    }
+
+    [HttpGet("getbyid")]
+    public IActionResult GetById (Guid id)
+    {
+        var result = _appointmentsService.GetAppointmentById(id);
+        return Ok(result);
+    }
+
+    [HttpDelete("delete")]
+    public IActionResult Delete(Guid id)
+    {
+        var result = _appointmentsService.DeleteAppointment(id);
+        return Ok(result);
+    }
+
+    [HttpPut("update")]
+    public IActionResult Update(Appointment appointment)
+    {
+        var result = _appointmentsService.UpdateAppointment(appointment);
+        return Ok(result);
+    }
+
+    public IActionResult GetAppointmentByDoctorId(int doctorId)
+    {
+        var result = _appointmentsService.GetAppointmentsByDoctorId(doctorId);
+        return Ok(result);
     }
 }
--- HospitalAppointment.WebApi/Controllers/DoctorsController.cs
+++ HospitalAppointment.WebApi/Controllers/DoctorsController.cs
@@ -1,11 +1,61 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HospitalAppointment.WebApi.Models;
+using HospitalAppointment.WebApi.Models.Enums;
+using HospitalAppointment.WebApi.Services.Abstracts;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HospitalAppointment.WebApi.Controllers
+namespace HospitalAppointment.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DoctorsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DoctorsController : ControllerBase
+    private IDoctorService _doctorService;
+
+    public DoctorsController(IDoctorService doctorService)
     {
+        _doctorService = doctorService;
+    }
+
+    [HttpGet("getall")]
+    public IActionResult GetAll()
+    {
+        var result = _doctorService.GetAllDoctors();
+        return Ok(result);
+    }
+
+    [HttpPost("add")]
+    public IActionResult Add(Doctor doctor)
+    {
+        var result = _doctorService.AddDoctor(doctor);
+        return Ok(result);
+    }
+
+    [HttpGet("getbyid")]
+    public IActionResult GetById(int id)
+    {
+        var result = _doctorService.GetDoctorById(id);
+        return Ok(result);
+    }
+
+    [HttpDelete("delete")]
+    public IActionResult Delete(int id)
+    {
+        var result = _doctorService.DeleteDoctor(id);
+        return Ok(result);
+    }
+
+    [HttpPut("update")]
+    public IActionResult Update(Doctor doctor)
+    {
+        var result = _doctorService.UpdateDoctor(doctor);
+        return Ok(result);
+    }
+
+    [HttpGet("getbybranch")]
+    public IActionResult GetByBranch(BranchType branch)
+    {
+        var result = _doctorService.GetDoctorsByBranch(branch);
+        return Ok(result);
     }
 }
